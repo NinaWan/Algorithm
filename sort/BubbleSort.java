9c0cdcdb25e1d93a624a48e093f147a1e304038d
Nina Wan <aileenwnn@gmail.com>
2022-01-13 21:00:23 +0800

    Add bubble sort and merge sort

---
+++ sort/BubbleSort.java
@@ -0,0 +1,16 @@
+package com.aileenw.sort;
+
+public class BubbleSort {
+    public void sort(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
---
+++ sort/MergeSort.java
@@ -0,0 +1,38 @@
+package com.aileenw.sort;
+
+public class MergeSort {
+    private void sort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = start + (end - start) / 2;
+        sort(nums, start, mid);
+        sort(nums, mid + 1, end);
+
+        merge(nums, start, mid, end);
+    }
+
+    private void merge(int[] nums, int start, int mid, int end) {
+        int[] res = new int[end - start + 1];
+        int l = start, r = mid + 1, i = 0;
+
+        while (l <= mid && r <= end) {
+            if (nums[l] < nums[r]) {
+                res[i++] = nums[l++];
+            } else {
+                res[i++] = nums[r++];
+            }
+        }
+
+        while (l <= mid) {
+            res[i++] = nums[l++];
+        }
+
+        while (r <= end) {
+            res[i++] = nums[r++];
+        }
+
+        System.arraycopy(res, 0, nums, start, end - start + 1);
+    }
+}
